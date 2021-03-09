c07169ccd2ce4d6188361801d7b50b3015398f1d
Kodai Takao (Fork HomeWorkstation) <kodai100@gmail.com>
2021-03-09 14:22:06 +0900

    add ping utility

---
+++ Assets/Scripts/0_Infrastructure/PingUtility.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Ping = System.Net.NetworkInformation.Ping;
+
+public class PingUtility
+{
+
+    public static async Task<bool> Ping(IPAddress address, int timeout = 5000)
+    {
+
+        var pingSender = new Ping();
+        
+        var buffer = Encoding.ASCII.GetBytes("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        var options = new PingOptions(64, true);
+        
+        var reply = await pingSender.SendPingAsync(address, timeout, buffer, options);
+
+        Debug.Log(
+            $"Reply from {reply.Address}: bytes={reply.Buffer.Length} time={reply.RoundtripTime}ms TTL={reply.Options.Ttl}");
+        
+        return reply.Status == IPStatus.Success;
+
+    }
+
+}
