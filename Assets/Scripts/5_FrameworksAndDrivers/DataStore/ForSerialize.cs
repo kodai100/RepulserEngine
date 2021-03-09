e34da32e6bc7c9b9c812c3fd4b0050d7367e4a9f
Kodai Takao (Fork HomeWorkstation) <kodai100@gmail.com>
2021-03-09 14:22:47 +0900

    bundle datastore serializers

---
+++ Assets/Scripts/5_FrameworksAndDrivers/DataStore/ForSerialize.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using ProjectBlue.RepulserEngine.Domain.Model;
+
+namespace ProjectBlue.RepulserEngine.DataStore
+{
+    [Serializable]
+    public class CommandSettingListForSerialize
+    {
+        public List<CommandSetting> Data = new List<CommandSetting>();
+
+        public CommandSettingListForSerialize(){}
+        
+        public CommandSettingListForSerialize(IEnumerable<CommandSetting> data)
+        {
+            
+            Data.Clear();
+            
+            foreach (var component in data)
+            {
+                Data.Add(component);
+            }
+        }
+    }
+
+    [Serializable]
+    public class EndpointSettingListForSerialize
+    {
+        public List<EndpointSetting> Data = new List<EndpointSetting>();
+
+        public EndpointSettingListForSerialize(){}
+        
+        public EndpointSettingListForSerialize(IEnumerable<EndpointSetting> data)
+        {
+            
+            Data.Clear();
+            
+            foreach (var component in data)
+            {
+                if(component == null) continue;
+                Data.Add(component);
+            }
+        }
+    }
+    
+    [Serializable]
+    public class TimecodeSettingListForSerialize
+    {
+        public List<TimecodeSetting> Data = new List<TimecodeSetting>();
+
+        public TimecodeSettingListForSerialize(){}
+        
+        public TimecodeSettingListForSerialize(IEnumerable<TimecodeSetting> data)
+        {
+            
+            Data.Clear();
+            
+            foreach (var component in data)
+            {
+                if(component == null) continue;
+                Data.Add(component);
+            }
+            
+        }
+    }
+
+
+}
