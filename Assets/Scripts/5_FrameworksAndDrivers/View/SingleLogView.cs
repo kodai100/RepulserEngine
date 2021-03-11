43b64a6326138195ee5f3ab55342d12bb3e0d550
Kodai Takao (Fork HomeWorkstation) <kodai100@gmail.com>
2021-03-11 19:15:16 +0900

    colorlize single log view

--- Assets/Scripts/5_FrameworksAndDrivers/View/SingleLogView.cs
+++ Assets/Scripts/5_FrameworksAndDrivers/View/SingleLogView.cs
@@ -27,6 +27,24 @@ namespace ProjectBlue.RepulserEngine.View
         private void OnLog(string logText, string stackTrace, LogType logType)
         {
             text.text = logText;
+
+            switch (logType)
+            {
+                case LogType.Log:
+                    text.color = Color.white;
+                    break;
+                case LogType.Warning:
+                    text.color = Color.yellow;
+                    break;
+                case LogType.Error:
+                case LogType.Exception:
+                case LogType.Assert:
+                    text.color = Color.red;
+                    break;
+                default:
+                    text.color = Color.white;
+                    break;
+            }
         }
     }
     
