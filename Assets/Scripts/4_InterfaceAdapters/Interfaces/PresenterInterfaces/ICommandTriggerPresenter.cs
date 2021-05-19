4ab617010960572098f6270c19971c2fa2f25778
nmxi <mikki.stad@gmail.com>
2021-05-19 16:49:29 +0900

    fix command can now be sent from trigger buttons

--- Assets/Scripts/4_InterfaceAdapters/Interfaces/PresenterInterfaces/ICommandTriggerPresenter.cs
+++ Assets/Scripts/4_InterfaceAdapters/Interfaces/PresenterInterfaces/ICommandTriggerPresenter.cs
@@ -1,9 +1,12 @@
 ﻿using ProjectBlue.RepulserEngine.Domain.DataModel;
+using System;
 
 namespace ProjectBlue.RepulserEngine.Presentation
 {
     public interface ICommandTriggerPresenter
     {
+        IObservable<CommandSetting> OnTriggerAsObservable { get; }
+
         void Send(CommandSetting command);
     }
 
