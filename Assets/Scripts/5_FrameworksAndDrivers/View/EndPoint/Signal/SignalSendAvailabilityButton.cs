ed9e73082dcadfda98c0e82a01361591627b05ac
Kodai Takao (Fork HomeWorkstation) <kodai100@gmail.com>
2021-06-15 07:14:15 +0900

    disable / enable text change in endpoint setting

--- Assets/Scripts/5_FrameworksAndDrivers/View/EndPoint/Signal/SignalSendAvailabilityButton.cs
+++ Assets/Scripts/5_FrameworksAndDrivers/View/EndPoint/Signal/SignalSendAvailabilityButton.cs
@@ -6,20 +6,20 @@ using UnityEngine.UI;
 
 namespace ProjectBlue.RepulserEngine.View
 {
-
     public class SignalSendAvailabilityButton : MonoBehaviour
     {
-
         private EndpointSettingViewModel endpointSettingviewModel;
 
         [SerializeField] private Button button;
         [SerializeField] private Image image;
-        
+
+        [SerializeField] private TMP_Text statusText;
+
         [SerializeField] private Color enabledColor = new Color(0f, 0.7f, 0.7f);
         [SerializeField] private Color disabledColor = new Color(0.7f, 0f, 0f);
-        
+
         [SerializeField] private TMP_Text indexText;
-        
+
         private void Start()
         {
             button.OnClickAsObservable().Subscribe(_ =>
@@ -28,20 +28,25 @@ namespace ProjectBlue.RepulserEngine.View
                 ChangeBackground(endpointSettingviewModel.ConnectionEnabled);
             }).AddTo(this);
         }
-        
+
         public void SetIndex(int index)
         {
             // image.color = failColor;
 
             if (indexText)
             {
-                indexText.text = (index+1).ToString();
+                indexText.text = (index + 1).ToString();
             }
         }
 
         private void ChangeBackground(bool enabled)
         {
             image.color = enabled ? enabledColor : disabledColor;
+
+            if (statusText)
+            {
+                statusText.text = enabled ? "Enabled" : "Disabled";
+            }
         }
 
         // set with button recreation
@@ -51,7 +56,5 @@ namespace ProjectBlue.RepulserEngine.View
 
             endpointSettingViewModel.connectionEnabled.Subscribe(ChangeBackground).AddTo(this);
         }
-
     }
-
 }
